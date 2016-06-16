d25f16beb69536365d3b7b18668023a6608baa7d
Damian Odoemena <damucci05@yahoo.com>
2016-06-15 21:26:37 -0400

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component'
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello Damian playing with angular</h1><courses></courses>',
+    directives: [CoursesComponent]
+})
+export class AppComponent { }
